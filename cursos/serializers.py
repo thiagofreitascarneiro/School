7930d815eead619c3050872d8f738641d67f6860
Thiago <thiagofreitascarneiro@hotmail.com>
2022-05-15 15:26:28 -0300

    create serializers

---
+++ cursos/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from .models import Course, Avaliation
+
+
+
+class AvaliationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Avaliation
+        fields = (
+            'id',
+            'course',
+            'name',
+            'email',
+            'comment',
+            'avaliation',
+            'create',
+            'active'
+        )
+    
+class CourseSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Course
+        fields = (
+            'id',
+            'title',
+            'url',
+            'create',
+            'active'
+        )
